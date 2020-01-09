bfca8f928a5228a67c85b55132f4e71ed5f78c1e
calumleask <cjleask@hotmail.co.uk>
2020-01-09 22:15:20 +0000

    Adding shaders for textures

--- src/graphics/shaders/sources/shader_sources.js
+++ src/graphics/shaders/sources/shader_sources.js
@@ -1,7 +1,9 @@
 import { basicVertexSource } from "~/graphics/shaders/sources/vertex/basic.js";
+import { texturedVertexSource } from "~/graphics/shaders/sources/vertex/textured.js";
 
 import { basicFragmentSource } from "~/graphics/shaders/sources/fragment/basic.js";
 import { coloredFragmentSource } from "~/graphics/shaders/sources/fragment/colored.js";
+import { texturedFragmentSource } from "~/graphics/shaders/sources/fragment/textured.js";
 
 // TODO: create a method
 export const shaderSources = {
@@ -12,5 +14,9 @@ export const shaderSources = {
     colored: {
         vertex: basicVertexSource,
         fragment: coloredFragmentSource
+    },
+    textured: {
+        vertex: texturedVertexSource,
+        fragment: texturedFragmentSource
     }
 };
