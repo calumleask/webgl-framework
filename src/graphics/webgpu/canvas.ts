382ade9006c737abc1675db367565dac2c918df9
calumleask <cjleask@hotmail.co.uk>
2021-04-23 16:38:46 +0100

    Simplifying the initialisation of the canvas

--- src/graphics/webgpu/canvas.ts
+++ src/graphics/webgpu/canvas.ts
@@ -4,22 +4,22 @@ export class WebGPUCanvas {
   private _canvas:  HTMLCanvasElement | null;
   private _context: GPUCanvasContext | null;
 
-  constructor() {
+  constructor(canvasId: string) {
     this._canvas = null;
     this._context = null;
+
+    this._init(canvasId);
   }
 
-  init(canvasId: string): void {
+  /** @interval */
+  private _init(canvasId: string): void {
     this._canvas = document.querySelector("#" + canvasId);
     // TODO: check this worked.
 
-    // Initialize the WebGPU context
     this._context = this._canvas ? this._canvas.getContext("gpupresent") : null;
 
-    // Only continue if WebGPU is available and working
     if (!this._context) {
       console.error("Unable to initialize WebGPU. It may not be supported/enabled in your browser.");
-      return;
     }
   }
 
