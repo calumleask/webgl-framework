1702bd0b39e075953617ae2c12480b48e585a82f
calumleask <cjleask@hotmail.co.uk>
2021-04-26 21:27:16 +0100

    Removing position based colour from the texture shaders

--- src/graphics/webgpu/shaders/sources/fragment/textured.ts
+++ src/graphics/webgpu/shaders/sources/fragment/textured.ts
@@ -4,8 +4,7 @@ export const texturedFragmentSource = `
   [[binding(2), group(0)]] var myTexture: texture_2d<f32>;
 
   [[stage(fragment)]]
-  fn main([[location(0)]] fragUV: vec2<f32>,
-          [[location(1)]] fragPosition: vec4<f32>) -> [[location(0)]] vec4<f32> {
-    return textureSample(myTexture, mySampler, fragUV) * fragPosition;
+  fn main([[location(0)]] fragUV: vec2<f32>) -> [[location(0)]] vec4<f32> {
+    return textureSample(myTexture, mySampler, fragUV);
   }
 `;
